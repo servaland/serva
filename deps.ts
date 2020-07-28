1eb61495bdf749e26ecccf6aa1f4d5da5395a81e
Christopher Turner <turner296@gmail.com>
2020-07-28 18:01:33 +1000

    updating std version

--- deps.ts
+++ deps.ts
@@ -1,12 +1,12 @@
-export * as flags from "https://deno.land/std@0.57.0/flags/mod.ts";
+export * as flags from "https://deno.land/std@0.62.0/flags/mod.ts";
 
 // fs contains unstable features just re-export under fs
-import { readJson } from "https://deno.land/std@0.57.0/fs/read_json.ts";
-import { walk } from "https://deno.land/std@0.57.0/fs/walk.ts";
+import { readJson } from "https://deno.land/std@0.62.0/fs/read_json.ts";
+import { walk } from "https://deno.land/std@0.62.0/fs/walk.ts";
 export const fs = { readJson, walk };
 
-export * as http from "https://deno.land/std@0.57.0/http/mod.ts";
+export * as http from "https://deno.land/std@0.62.0/http/mod.ts";
 
-export * as path from "https://deno.land/std@0.57.0/path/mod.ts";
+export * as path from "https://deno.land/std@0.62.0/path/mod.ts";
 
 export * as pathToRegexp from "https://raw.githubusercontent.com/pillarjs/path-to-regexp/v6.1.0/src/index.ts";
